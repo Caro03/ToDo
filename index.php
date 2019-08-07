7a2dbb6ebed452242eba46ad30c06bdb0993cbee
Caro03 <caroverstreken@hotmail.com>
2019-08-07 18:11:37 +0200

    add basic html

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Todo</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="bg-header">
+    <h1 class="h-welkom">Welkom bij de Todo app</h1>
+    <h2 class="h-text">Voeg hier al jouw taken in die je moet vervullen</h2>
+    </div>
+    <div class="bg-index">
+                <div class="form__post">
+					<input type="text" placeholder="Welke taak moet je volbrengen?" id="post" name="post">
+                </div>
+                <div class="form__field">
+                    <input type="submit" value="Post it!" class="btn btn--primary"> 
+                </div> 
+                </div>
+</body>
+</html>
