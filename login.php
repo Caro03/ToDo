025e9da703520abfc9a1dc35216d3646f91b7655
Caro03 <caroverstreken@hotmail.com>
2019-08-07 17:42:30 +0200

    add styling to login file

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Todo</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body class="bg-foto">
+<div class="bg-login">
+<h1 class="register">Login</h1>
+				<div class="form__field">
+					<input type="text" placeholder="Username" id="username" name="username">
+				</div>
+				<div class="form__field">
+					<input type="password" placeholder="Password" id="password" name="password">
+				</div>
+
+				<div class="form__field">
+					<input type="submit" value="Sign me in!" class="btn btn--primary">	
+					<p class="no-account">Not an account yet? <a href="register.php" class="a-sign">Sign up here</a></p>	
+				</div>
+                </div>
+</body>
+</html>
