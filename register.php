caf74ff210505a4c6bda5586ed95bc1c1c5cd08a
Caro03 <caroverstreken@hotmail.com>
2019-08-07 17:41:58 +0200

    add styling to register file

---
+++ register.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Todo</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body class="bg-foto">
+<div class="bg-register">
+<h1 class="register">Register</h1>
+                <div class="form__field">
+					<input type="text" placeholder="First name" id="firstName" name="firstName">
+				</div>
+				<div class="form__field">
+					<input type="text" placeholder="Last name" id="lastName" name="lastName">
+				</div>
+				<div class="form__field">
+					<input type="text" placeholder="Email" id="email" name="email">
+				</div>
+				<div class="form__field">
+					<input type="text" placeholder="Username" id="username" name="username">
+				</div>
+				<div class="form__field">
+					<input type="password" placeholder="Password" id="password" name="password">
+				</div>
+
+                <div class="form__field">
+					<input type="password" placeholder="Repeat password" id="password_confirmation" name="password_confirmation">
+				</div>
+
+				<div class="form__field">
+					<input type="submit" value="Sign me up!" class="btn btn--primary">	
+					<p class="no-account">Already registered? <a href="login.php" class="a-sign">Sign in here</a></p>	
+				</div>
+                </div>
+</body>
+</html>
